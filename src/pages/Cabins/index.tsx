8995a89553401fdbb9d1e59167296751bceffd52
NaranjaX <naranjax@MAC-68439.local>
2023-08-14 19:52:55 -0300

    fix image url in cabin

--- src/pages/Cabins/index.tsx
+++ src/pages/Cabins/index.tsx
@@ -67,8 +67,8 @@ const CabinsPage: React.FC = () => {
                   <div className="flex flex-col">
                     <Img
                       className="mx-auto object-cover z-10"
-                      src="images/banner_cabinas.jpg"
-                      alt="bgfiguraonda"
+                      src="/images/banner_cabinas.jpg"
+                      alt="banner cabinas"
                     />
                   </div>
               </div>
